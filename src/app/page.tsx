7ca0349e559bcccd0ef3a431a9cf0c9175243e2e
Andre Camerino <camerinoandre@gmail.com>
2025-08-11 15:31:28 +1200

    add animation button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import BasicScene from "@/components/three/BasicScene";
 import AnimationButton from "@/components/AnimationButton";
 
 export default function Page() {
+  const [isAnimating, setIsAnimating] = useState(false);
+
   return (
     <>
       <div>
-        <BasicScene />
+        <BasicScene isAnimating={isAnimating}/>
       </div>
 
       <div className="fixed bottom-5 left-1/2 -translate-x-1/2">
-        <AnimationButton>Click Me</AnimationButton>
+        <AnimationButton onClick={() => setIsAnimating(true)}>Lets get started</AnimationButton>
       </div>
     </>
   );
